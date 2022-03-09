7855ce6c6b9e0923b161a01b4c4716dd2325faba
Camille Semaan <csemaan@cmaisonneuve.qc.ca>
2022-03-09 14:13:32 -0500

    Première intégration de Firebase Firestore

---
+++ src/firebase/init.js
@@ -0,0 +1,9 @@
+import firebaseConfig from './config';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from "firebase/firestore";
+
+// Initialiser Firebase
+export const instanceFirebase = initializeApp(firebaseConfig);
+
+// Initialiser Firestore
+export const bdFirestore = getFirestore();
